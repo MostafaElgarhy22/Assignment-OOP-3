fa9f87a358d71bc4abb9facf2703c2132dfc7fa9
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-05 23:16:28 +0200

    Q 1

--- Assignment OOP 3/Program.cs
+++ Assignment OOP 3/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        public class Calculator
+        {
+            #region Part 1 Question 1
+            //public int Add(int a, int b)
+            //{
+            //    return a + b;
+            //}
+
+            //public int Add(int a, int b, int c)
+            //{
+            //    return a + b + c;
+            //}
+
+            //public double Add(double a, double b)
+            //{
+            //    return a + b;
+            //}
+            #endregion
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 1 Question 1
+            //Calculator calculator = new Calculator();
+            //Console.WriteLine(calculator.Add(6, 4));
+            //Console.WriteLine(calculator.Add(4, 2, 5));
+            //Console.WriteLine(calculator.Add(2.5, 4.7));
+            #endregion
+            
         }
     }
 }
